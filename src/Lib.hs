8ae68b6e469399be68d495089199b959b49d7b5d
chris-bacon <chrisbacon2009@hotmail.com>
2018-12-16 18:23:41 +0000

    Begin

---
+++ src/Lib.hs
@@ -0,0 +1,61 @@
+module Lib where
+
+import GHC.Generics
+import Aws.Lambda.Runtime
+import qualified Data.Aeson as Aeson
+
+data Event = Event QueryStrings deriving (Generic, Show)
+
+data QueryStrings = QueryStrings 
+    { personName :: String 
+    } deriving (Generic, Show)
+
+data Response = Response
+  { statusCode :: Int
+  , body :: String
+  } deriving (Generic)
+
+instance Aeson.FromJSON Event
+instance Aeson.ToJSON Event
+instance Aeson.FromJSON QueryStrings
+instance Aeson.ToJSON QueryStrings
+instance Aeson.FromJSON Response
+instance Aeson.ToJSON Response
+
+getQueryStrings :: Aeson.Value -> Maybe QueryStrings
+getQueryStrings = Aeson.decode . Aeson.encode
+
+data WebPage = WebPage 
+    { head       :: String
+    , bodyInLine :: String
+    , last       :: String
+    } deriving (Show)
+
+class Html a where
+    showHtml      :: a -> String
+    getHead       :: a -> String
+    getBodyInLine :: a -> String
+    getLast       :: a -> String
+
+instance Html WebPage where
+    showHtml      (WebPage a b c) = a ++ b ++ c
+    getHead       (WebPage a _ _) = a
+    getBodyInLine (WebPage _ b _) = b
+    getLast       (WebPage _ _ c) = c
+
+constructWebPage :: QueryStrings -> WebPage
+constructWebPage qs = WebPage h b l
+    where
+        h = "<head></head>"
+        b = "<h1>Hello there! " ++ personName qs ++ "</h1>"
+        l = ""
+
+handler :: Aeson.Value -> Context -> IO (Either String String)
+handler event _ = do
+    let person  = getQueryStrings event
+    case person of 
+      Just p  -> do
+          let webpage = constructWebPage p
+          return $ Right $ showHtml webpage
+      Nothing -> return $ Left "Please provide only a personName query string"
+
